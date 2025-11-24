e37aca1d44a40ea5cfb6d35abe2cb413a143d3ff
DeveloperKairo <contatodevkairo@gmail.com>
2025-11-24 20:21:48 -0300

    feat: global configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import './App.css'
+
+import StartScreen from './components/StartScreen/StartScreen';
+
 function App() {
 
   return (
     <div className="App">
-      
+      <StartScreen />
     </div>
   )
 }
